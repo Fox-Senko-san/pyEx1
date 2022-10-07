94eb20c0819a1cd246873476a013d996e5dc085a
Senko san <nani.steam.dmitriy@gmail.com>
2022-10-07 23:58:19 +0700

    Скрипт для чтения файла с использованием PrettyTable

---
+++ Ex2.2.py
@@ -0,0 +1,19 @@
+# With using prettyTable
+from prettytable import PrettyTable
+
+class EmptyFileError(Exception):
+    pass
+
+try:
+    with open("file.txt", "r", encoding="UTF-8") as f:
+        data = [line.strip().split() for line in f]
+    if not data: raise EmptyFileError
+
+    table = PrettyTable(["Имя", "Фамилия", "Отчество", "Дата рождения"])
+    for line in data:
+        table.add_row(line)
+    print(table)
+
+except FileNotFoundError: print("Файл не найден!")
+except EmptyFileError: print("Файл пуст!")
+except UnicodeError: print("Ошибка кодировки!")
